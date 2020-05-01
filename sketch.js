fd36b60f5f58a8b760077f762b626132c00a1e84
ujjessha <64350773+ujjessha@users.noreply.github.com>
2020-05-01 20:07:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,6 @@ function draw() {
   
   movingRect.x=World.mouseX;
   movingRect.y=World.mouseY;
-
   if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 
     && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
     && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
